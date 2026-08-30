ficd0/kakoune-lsp

commit 2c44d2847f1d4138f443c28489c5204bd9875ab2

Refactor didSave capability check

src/text_sync.rs
@@ -99,21 +99,23 @@ pub fn text_document_did_close(meta: EditorMeta, ctx: &mut Context) {
 pub fn text_document_did_save(meta: EditorMeta, ctx: &mut Context) {
     for &server_id in &meta.servers {
         let server = ctx.server(server_id);
-        let options = match &server.capabilities.as_ref().unwrap().text_document_sync {
-            Some(TextDocumentSyncCapability::Options(TextDocumentSyncOptions {
-                save: Some(opts),
-                ..
-            })) if !matches!(opts, TextDocumentSyncSaveOptions::Supported(false)) => opts,
-            _ => continue, // don't send didSave by default
+        let include_text = match &server.capabilities.as_ref().unwrap().text_document_sync {
+            Some(TextDocumentSyncCapability::Options(TextDocumentSyncOptions { save, .. })) => {
+                match save {
+                    Some(TextDocumentSyncSaveOptions::SaveOptions(SaveOptions {
+                        include_text: Some(include_text),
+                    })) => *include_text,
+                    _ => continue,
+                }
+            }
+            _ => continue,
         };
-        let text = match options {
-            TextDocumentSyncSaveOptions::SaveOptions(SaveOptions {
-                include_text: Some(true),
-            }) => ctx
-                .documents
+        let text = if include_text {
+            ctx.documents
                 .get(&meta.buffile)
-                .map(|doc| doc.text.to_string()),
-            _ => None,
+                .map(|doc| doc.text.to_string())
+        } else {
+            None
         };
 
         let uri = file_path_to_uri(&meta.buffile);

commit 1735785a93929a1d3a25211da247802d90f9e87d

fix didSave capability check regression
Fixes a regression introduced in 2c44d28 that caused `"save":{}` to be
treated as lacking save support entirely. Fixes the issue with
rust-analyzer diagnostics not refreshing correctly on save.

src/text_sync.rs
@@ -96,21 +96,27 @@ pub fn text_document_did_close(meta: EditorMeta, ctx: &mut Context) {
     }
 }
 
+fn did_save_include_text(text_document_sync: &Option<TextDocumentSyncCapability>) -> Option<bool> {
+    match text_document_sync {
+        Some(TextDocumentSyncCapability::Options(TextDocumentSyncOptions {
+            save: Some(TextDocumentSyncSaveOptions::Supported(true)),
+            ..
+        })) => Some(false),
+        Some(TextDocumentSyncCapability::Options(TextDocumentSyncOptions {
+            save: Some(TextDocumentSyncSaveOptions::SaveOptions(options)),
+            ..
+        })) => Some(options.include_text.unwrap_or(false)),
+        _ => None,
+    }
+}
+
 pub fn text_document_did_save(meta: EditorMeta, ctx: &mut Context) {
     for &server_id in &meta.servers {
         let server = ctx.server(server_id);
-        let include_text = match &server.capabilities.as_ref().unwrap().text_document_sync {
-            Some(TextDocumentSyncCapability::Options(TextDocumentSyncOptions {
-                save: Some(save),
-                ..
-            })) => match save {
-                TextDocumentSyncSaveOptions::SaveOptions(SaveOptions { include_text }) => {
-                    include_text.clone().unwrap_or(false)
-                }
-                TextDocumentSyncSaveOptions::Supported(true) => false,
-                TextDocumentSyncSaveOptions::Supported(false) => continue,
-            },
-            _ => continue,
+        let Some(include_text) =
+            did_save_include_text(&server.capabilities.as_ref().unwrap().text_document_sync)
+        else {
+            continue;
         };
         let text = if include_text {
             ctx.documents
@@ -316,5 +322,40 @@ pub fn register_workspace_did_change_watched_files(
             ))
             .or_default()
             .push(CompiledFileSystemWatcher { kind, pattern });
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    fn include_text(capabilities: &str) -> Option<bool> {
+        let capabilities: ServerCapabilities = serde_json::from_str(capabilities).unwrap();
+        did_save_include_text(&capabilities.text_document_sync)
+    }
+
+    #[test]
+    fn did_save_capability() {
+        // no save notification
+        assert_eq!(include_text("{}"), None);
+        assert_eq!(include_text(r#"{"textDocumentSync":{"save":false}}"#), None);
+        // set with boolean
+        assert_eq!(
+            include_text(r#"{"textDocumentSync":{"save":true}}"#),
+            Some(false)
+        );
+        // empty options is same as false
+        assert_eq!(
+            include_text(r#"{"textDocumentSync":{"save":{}}}"#),
+            Some(false)
+        );
+        // explicit includeText settings
+        assert_eq!(
+            include_text(r#"{"textDocumentSync":{"save":{"includeText":false}}}"#),
+            Some(false)
+        );
+        assert_eq!(
+            include_text(r#"{"textDocumentSync":{"save":{"includeText":true}}}"#),
+            Some(true)
+        );
     }
 }